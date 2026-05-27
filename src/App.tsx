0a349e6c1e9c76425a4ece80853e0bf2fac9d90e
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-27 16:38:31 +0900

    feat: 라우팅 기능 구현 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import DefaultLayout from "./components/layout/DefaultLayout";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="min-h-screen bg-[#e5e7eb] flex justify-center">
       <div className="min-h-screen w-[393px] overflow-hidden bg-white">
         <Header />
+        <DefaultLayout />
         <Footer />
       </div>
     </div>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,44 +6,73 @@ import chatIcon from "@/assets/icons/footer/chat-bubble.png";
 import chatActiveIcon from "@/assets/icons/footer/chat-bubble-active.png";
 import myIcon from "@/assets/icons/footer/person.png";
 import myActiveIcon from "@/assets/icons/footer/person-active.png";
-import { useState } from "react";
+import { ROUTES } from "../../constants/routes";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const navItems = [
-  { id: 0, label: "홈", icon: homeIcon, iconActive: homeActiveIcon },
-  { id: 1, label: "도움요청", icon: sirenIcon, iconActive: sirenActiveIcon },
-  { id: 2, label: "채팅", icon: chatIcon, iconActive: chatActiveIcon },
-  { id: 3, label: "내정보", icon: myIcon, iconActive: myActiveIcon },
+  {
+    id: 0,
+    label: "홈",
+    icon: homeIcon,
+    iconActive: homeActiveIcon,
+    path: ROUTES.HOME,
+  },
+  {
+    id: 1,
+    label: "도움요청",
+    icon: sirenIcon,
+    iconActive: sirenActiveIcon,
+    path: ROUTES.HELP,
+  },
+  {
+    id: 2,
+    label: "채팅",
+    icon: chatIcon,
+    iconActive: chatActiveIcon,
+    path: ROUTES.CHAT,
+  },
+  {
+    id: 3,
+    label: "내정보",
+    icon: myIcon,
+    iconActive: myActiveIcon,
+    path: ROUTES.MYPAGE,
+  },
 ];
 
 const Footer = () => {
-  const [currentPage, setCurrentPage] = useState(0);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   return (
-    <>
-      <nav className="fixed bottom-0 w-[393px] bg-white px-1 py-2 border-t-1  border-lightgray">
-        <ul className="grid grid-cols-4 gap-4">
-          {navItems.map((item) => (
+    <nav className="fixed bottom-0 w-[393px] bg-white px-1 py-2 border-t border-lightgray">
+      <ul className="grid grid-cols-4 gap-4">
+        {navItems.map((item) => {
+          const isActive = location.pathname === item.path;
+
+          return (
             <li
               key={item.id}
-              onClick={() => setCurrentPage(item.id)}
+              onClick={() => navigate(item.path)}
               className="flex cursor-pointer flex-col items-center justify-center gap-1"
             >
               <img
-                src={item.id === currentPage ? item.iconActive : item.icon}
-                className={`
-                    h-[28px] w-[28px] object-contain`}
+                src={isActive ? item.iconActive : item.icon}
+                className="h-[28px] w-[28px] object-contain"
               />
 
               <small
-                className={`${item.id === currentPage ? "text-primary" : "text-darkgray"} text-xs`}
+                className={`text-xs ${
+                  isActive ? "text-primary" : "text-darkgray"
+                }`}
               >
                 {item.label}
               </small>
             </li>
-          ))}
-        </ul>
-      </nav>
-    </>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
