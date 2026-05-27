233bc86e4512cba5ee18e70325d37bf4bc8ea7b8
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-27 15:05:30 +0900

    fix: main.css 경로 참조 오류 수정 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen bg-[#e5e7eb] flex justify-center">
-      <div className="min-h-screen w-[393px] overflow-hidden bg-white">
-        <h1>안녕하세요</h1>
-      </div>
+      <div className="min-h-screen w-[393px] overflow-hidden bg-white"></div>
     </div>
   );
 }
