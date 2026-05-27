660e61b72d6ab8d027f9245df3cdcc7e71f82aff
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-27 16:03:32 +0900

    feat: 헤더 구현 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="min-h-screen bg-[#e5e7eb] flex justify-center">
       <div className="min-h-screen w-[393px] overflow-hidden bg-white">
+        <Header />
         <Footer />
       </div>
     </div>
