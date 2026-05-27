ca37574fa3849e6e802765f5e58e10bda9419f2d
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-27 14:59:25 +0900

    feat: 웹앱 기본 프레임 구현 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen bg-[#e5e7eb] flex justify-center">
+      <div className="min-h-screen w-[393px] overflow-hidden bg-white">
+        <h1>안녕하세요</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
