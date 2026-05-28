b184361d76061696ca5fd76a7926566b0b04a379
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-28 18:08:38 +0900

    chore: 도움요청 페이지에서 헤더 숨김 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import DefaultLayout from "./components/layout/DefaultLayout";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
+import { useLocation } from 'react-router-dom';
+import DefaultLayout from './components/layout/DefaultLayout';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import { ROUTES } from './constants/routes';
 
 function App() {
+  const location = useLocation();
+  const isHelpPage = location.pathname === ROUTES.HELP;
+
   return (
     <div className="min-h-screen bg-[#e5e7eb] flex justify-center">
       <div className="min-h-screen w-[393px] overflow-hidden bg-white">
-        <Header />
+        {!isHelpPage && <Header />}
         <DefaultLayout />
         <Footer />
       </div>
