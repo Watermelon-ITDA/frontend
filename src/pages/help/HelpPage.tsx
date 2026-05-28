93975724c1415346ec5ac71b07bdf0a90ea385b6
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-28 18:05:49 +0900

    Feat: 카카오맵 API 연동 (#3)

--- src/pages/help/HelpPage.tsx
+++ src/pages/help/HelpPage.tsx
@@ -1,7 +1,52 @@
+import { loadKakaoScript } from '@/utils/loadMap';
+import { useEffect, useState } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
 const HelpPage = () => {
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
+
+  const [currentPosition, setCurrentPosition] = useState({
+    lat: 37.5665,
+    lng: 126.978,
+  });
+
+  useEffect(() => {
+    const initMap = async () => {
+      await loadKakaoScript();
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCurrentPosition({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+
+          setIsMapLoaded(true);
+        },
+        () => {
+          // 위치 권한 거부 시 기본 위치
+          setIsMapLoaded(true);
+        },
+      );
+    };
+
+    initMap();
+  }, []);
+
   return (
     <main className="pt-header">
-      <h1 className="text-xl font-bold">HELP PAGE</h1>
+      {isMapLoaded && (
+        <Map
+          center={currentPosition}
+          style={{
+            width: '100%',
+            height: '100vh',
+          }}
+          level={3}
+        >
+          <MapMarker position={currentPosition} />
+        </Map>
+      )}
     </main>
   );
 };
