aeff6308d72439e19a60f3a57466c86a11ce77f5
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-28 19:48:28 +0900

    design: Input Padding Y값 수정 (#3)

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -13,7 +13,7 @@ const Input = ({ label, error, className, ...props }: InputProps) => {
 
       <input
         className={cn(
-          'w-full rounded-md border px-3 py-2 text-sm outline-none transition',
+          'w-full rounded-md border px-3 py-3 text-sm outline-none transition',
           'border-lightgray focus:border-primary bg-white',
           error && 'border-red-400',
           className,
