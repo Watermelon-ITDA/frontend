2f71072393e8305d3e9a62def7daa1f202149643
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-29 20:09:02 +0900

    feat: 로그인 라우팅 추가 (#4)

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import { ROUTES } from "../../constants/routes";
-import HomePage from "../../pages/home/HomePage";
-import HelpPage from "../../pages/help/HelpPage";
-import ChatPage from "../../pages/chat/ChatPage";
-import MyPage from "../../pages/mypage/MyPage";
+import { Route, Routes } from 'react-router-dom';
+import { ROUTES } from '../../constants/routes';
+import HomePage from '../../pages/home/HomePage';
+import HelpPage from '../../pages/help/HelpPage';
+import ChatPage from '../../pages/chat/ChatPage';
+import MyPage from '../../pages/mypage/MyPage';
+import LoginPage from '@/pages/auth/LoginPage';
 
 const DefaultLayout = () => {
   return (
     <>
       <Routes>
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
         <Route path={ROUTES.HOME} element={<HomePage />} />
         <Route path={ROUTES.HELP} element={<HelpPage />} />
         <Route path={ROUTES.CHAT} element={<ChatPage />} />
