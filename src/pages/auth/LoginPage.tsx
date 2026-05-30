37443601388800e0d0564e1652fbb9f8d7de5a04
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-30 16:16:41 +0900

    refactor: 로그인 페이지 구조 통합 (#7)

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,21 +1,14 @@
 import Button from '@/components/common/Button';
-import googleLogo from '@/assets/icons/auth/google-color.svg';
-import { useGoogleLogin } from '@react-oauth/google';
 import { useState } from 'react';
 import Modal from '@/components/common/Modal';
 
 const LoginPage = () => {
   const [showErrorModal, setShowErrorModal] = useState(false);
 
-  const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-    //   console.log(tokenResponse);
-    },
+  const handleGoogleLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  };
 
-    onError: () => {
-      setShowErrorModal(true);
-    },
-  });
   return (
     <>
       <main className="relative h-full px-7">
@@ -32,12 +25,26 @@ const LoginPage = () => {
         <div className="absolute bottom-40 left-7 right-7 grid text-center gap-4">
           <Button
             className="w-full py-3 text-md flex justify-start gap-3"
-            onClick={() => login()}
+            onClick={() => handleGoogleLogin()}
           >
-            <img
-              src={googleLogo}
-              className="w-[20px] h-[20px] object-contain"
-            />
+            <svg width="18" height="18" viewBox="0 0 18 18">
+              <path
+                fill="#4285F4"
+                d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 002.38-5.88c0-.57-.05-.66-.15-1.18z"
+              />
+              <path
+                fill="#34A853"
+                d="M8.98 17c2.16 0 3.97-.72 5.3-1.94l-2.6-2a4.8 4.8 0 01-7.18-2.54H1.83v2.07A8 8 0 008.98 17z"
+              />
+              <path
+                fill="#FBBC05"
+                d="M4.5 10.52a4.8 4.8 0 010-3.04V5.41H1.83a8 8 0 000 7.18l2.67-2.07z"
+              />
+              <path
+                fill="#EA4335"
+                d="M8.98 4.18c1.17 0 2.23.4 3.06 1.2l2.3-2.3A8 8 0 001.83 5.4L4.5 7.49a4.77 4.77 0 014.48-3.31z"
+              />
+            </svg>
             Google로 계속하기
           </Button>
         </div>
