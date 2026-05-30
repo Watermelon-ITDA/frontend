071494c2b329371e2de7c735a0eab0ab714cd117
SuyeonShinnnn <suyeons5536@gmail.com>
2026-05-30 16:10:23 +0900

    refactor: 채팅 페이지 구조 정리 (#7)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,5 +3,7 @@ export const ROUTES = {
   HOME: '/',
   HELP: '/help',
   CHAT: '/chat',
+  CHATROOM_PATH: '/chat/:roomId',
+  CHATROOM: (roomId: string) => `/chat/${roomId}`,
   MYPAGE: '/mypage',
 };
